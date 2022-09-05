62ec934ab25eac2b82eac9d8f8d3f1563d89856b
Andrei <g.andmaximov@gmail.com>
2022-09-05 16:40:38 +0300

    solved: gateway problem maven build

--- gateway/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -4,15 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import ru.practicum.shareit.ShareItGateway;
 
 import java.nio.charset.StandardCharsets;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ContextConfiguration(classes = ShareItGateway.class)
 @WebMvcTest(controllers = UserController.class)
 public class UserControllerTest {
 
@@ -21,6 +25,9 @@ public class UserControllerTest {
 
     @Autowired
     private ObjectMapper mapper;
+
+    @MockBean
+    UserClient userClient;
 
     User user = new User(1L, "username", "email@ya.ru", null);
 
